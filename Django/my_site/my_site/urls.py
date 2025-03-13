300b6c49c2ac6e31e5d1eb4d21c7de2d91f9343a
blllinded <161970837+blllinded@users.noreply.github.com>
2025-03-13 09:58:05 +0100

    Dodano pliki dla LAB-3

--- Django/my_site/my_site/urls.py
+++ Django/my_site/my_site/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from polls import views  # Импортируем views из polls
 
 urlpatterns = [
-    path("", views.index, name="home"), 
-    path("polls/", include("polls.urls")),  
-    path("admin/", admin.site.urls),  
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
